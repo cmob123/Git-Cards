bdf2b58ba37f18572d9e90e69ecc5d7079450421
Christopher O'Brien <christopher.obrien@galenhealthcare.com>
2017-06-22 13:24:38 -0400

    First commit

---
+++ app/components/App.js
@@ -0,0 +1,9 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+class hereGoes extends React.Component{
+	render() {
+		return <h1>First React!</h1>;
+	}
+};
+
+module.exports = hereGoes;
