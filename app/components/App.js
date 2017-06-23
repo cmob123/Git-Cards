73cb8060cf8e906662f82792feeb14c540e3afce
Chris O'Brien <cobrien3@mail.smcvt.edu>
2017-06-23 11:59:36 -0400

    finished

--- app/components/App.js
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-class hereGoes extends React.Component{
-	render() {
-		return <h1>First React!</h1>;
-	}
-};
-
-module.exports = hereGoes;
